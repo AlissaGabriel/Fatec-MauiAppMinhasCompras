0dada0dafd859a8b8f6c0e3edd2f419dbf84b1a1
AlissaGabriel <alissagabriel14@gmail.com>
2026-04-10 09:04:16 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAppMinhasCompras
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            Window w = new Window(new AppShell());
+            w.Width = 400;
+            w.Height = 800;
+            return w;
+        }
+    }
+}
