d94d53f58bc5c41c2295186bbfceaef6ca0a5807
AlissaGabriel <alissagabriel14@gmail.com>
2026-04-14 16:24:50 -0300

    feat(products): add create, update, delete, list and total sum features.

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Globalization;
+using MauiAppMinhasCompras.Helpers;
 
 namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper? _db;
+
+        public static SQLiteDatabaseHelper Db 
+        {
+            get 
+            {
+                if (_db == null)
+                {
+                    string caminho_do_arquivo = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3"
+                            );
+
+                    _db = new SQLiteDatabaseHelper(caminho_do_arquivo);
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("pt-BR");
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
